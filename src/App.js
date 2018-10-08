bcc87f4e0b7ccedc5f790594dd9e3d25a36ec99b
kszymczycha <code2@smallbs.pl>
2018-10-08 18:46:07 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Game from './components/Game';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <>
+                    <Route path="/" component={Home} exact />
+                    <Route path="/game" component={Game} />
+                </>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
