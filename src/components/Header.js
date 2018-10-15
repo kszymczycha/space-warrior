c66318ec47e3aa265e4f8ab2fd5c5b89da42ddee
kszymczycha <kamilszymczycha@gmail.com>
2018-10-15 20:28:10 +0200

    Header. Scss variables file

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../scss/Header.scss';
+
+export default () => {
+    return (
+        <div className="header">
+            <h1>SPACE WARIOR</h1>
+        </div>
+    )
+}
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Header from './Header';
 import '../scss/Menu.scss';
 
 export default () => {
     return (
         <div className="menu">
-            <h1>  
+            <Header />
+            <h2>  
                 <Link className="startGameBtn" to="/game">Start Game</Link>
-            </h1>
+            </h2>
         </div>
     )
 }
