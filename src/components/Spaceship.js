a10931d6731f6f96d1264e29d1e5f44b4866b1e6
kszymczycha <kamilszymczycha@gmail.com>
2018-10-16 21:45:20 +0200

    Spaceship (player) start position

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -8,22 +8,31 @@ export default class Spaceship extends Component {
         super(props);
         this.canvasRef = React.createRef();
     }
+    
+    startPosition(windowInnerWidth, windowInnetHeight, width, height) {
+        const marginBottom = 10;
+        const startX = (windowInnerWidth / 2) - (width / 2);
+        const startY = windowInnetHeight - height - marginBottom;
+        return { startX, startY }
+    }
 
     componentDidMount() {
         const canvas = this.canvasRef.current;
         const ctx = canvas.getContext('2d');
         const imgSprite = new Image();
+        const windowInnerWidth = window.innerWidth;
+        const windowInnetHeight = window.innerHeight;
         canvas.style.marginTop = -window.innerHeight + 'px';
         canvas.style.display = 'block';
-        ctx.canvas.width = window.innerWidth;
-        ctx.canvas.height = window.innerHeight;
-
+        ctx.canvas.width = windowInnerWidth;
+        ctx.canvas.height = windowInnetHeight;
         imgSprite.src = img;
         imgSprite.onload = () => {
             const element = spriteElement('playerShip2_blue');
             element.then(data => {
-                const { x, y, width, height } = data.$;
-                ctx.drawImage(imgSprite, x, y, width, height, 0, 0, width, height);
+                const { x, y, width, height} = data.$;
+                const { startX , startY } = this.startPosition(windowInnerWidth, windowInnetHeight, width, height);
+                ctx.drawImage(imgSprite, x, y, width, height, startX, startY, width, height);
             });
         }
     }
